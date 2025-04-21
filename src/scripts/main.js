165cee24febc1bb5b3d162237615afc5b8e82fbe
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-21 18:04:16 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -357,7 +357,7 @@ const people = [
 // eslint-disable-next-line no-console
 console.log(people); // you can remove it
 
-let table = document.querySelector('.dashboard');
+let table = document.querySelector('.dashboard tbody');
 
 console.log(table);
 
